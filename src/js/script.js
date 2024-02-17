3465eae01d3291afe8d6895c8f0c045fb39e017c
Kuba1507 <kuba.romanski@onet.com.pl>
2024-02-17 11:22:24 +0100

    scripted cta button

--- src/js/script.js
+++ src/js/script.js
@@ -1,46 +1,51 @@
-const burgerBtn = document.querySelector(".nav__burger-btn");
-const navigation = document.querySelector(".nav__navbar");
-const navItems = document.querySelectorAll(".nav__navbar-item-link");
+const burgerBtn = document.querySelector('.nav__burger-btn')
+const navigation = document.querySelector('.nav__navbar')
+const navItems = document.querySelectorAll('.nav__navbar-item-link')
+const ctaButton = document.querySelector('.header__box-cta-button')
+const offersSection = document.querySelector('.offers')
 
-let filterItem = document.querySelector(".items-links");
-let filteredImages = document.querySelectorAll(".project-img");
+let filterItem = document.querySelector('.items-links')
+let filteredImages = document.querySelectorAll('.project-img')
 
-const actualYear = document.querySelector(".actual-year");
+const actualYear = document.querySelector('.actual-year')
 
 const handleNav = () => {
-	navigation.classList.toggle("show-mobile");
-};
+	navigation.classList.toggle('show-mobile')
+}
 
 const closeNav = () => {
-	navigation.classList.remove("show-mobile");
-};
+	navigation.classList.remove('show-mobile')
+}
 
 const updateYear = () => {
-	const year = new Date().getFullYear();
-	actualYear.textContent = year;
-};
+	const year = new Date().getFullYear()
+	actualYear.textContent = year
+}
 
-burgerBtn.addEventListener("click", handleNav);
-navItems.forEach((item) => {
-	item.addEventListener("click", closeNav);
-});
-document.addEventListener("DOMContentLoaded", () => {
-	updateYear();
-});
-window.addEventListener("load", () => {
-	filterItem.addEventListener("click", (selectedItem) => {
-		if (selectedItem.target.classList.contains("items-links__link")) {
-			document.querySelector(".menu-active").classList.remove("menu-active");
-			selectedItem.target.classList.add("menu-active");
-			let filterName = selectedItem.target.getAttribute("data-name");
-			filteredImages.forEach((image) => {
-				let filterImages = image.getAttribute("data-name");
-				if (filterImages == filterName || filterName == "all") {
-					image.style.display = "block";
+burgerBtn.addEventListener('click', handleNav)
+navItems.forEach(item => {
+	item.addEventListener('click', closeNav)
+})
+ctaButton.addEventListener('click', () => {
+	offersSection.scrollIntoView({ behavior: 'smooth' })
+})
+document.addEventListener('DOMContentLoaded', () => {
+	updateYear()
+})
+window.addEventListener('load', () => {
+	filterItem.addEventListener('click', selectedItem => {
+		if (selectedItem.target.classList.contains('items-links__link')) {
+			document.querySelector('.menu-active').classList.remove('menu-active')
+			selectedItem.target.classList.add('menu-active')
+			let filterName = selectedItem.target.getAttribute('data-name')
+			filteredImages.forEach(image => {
+				let filterImages = image.getAttribute('data-name')
+				if (filterImages == filterName || filterName == 'all') {
+					image.style.display = 'block'
 				} else {
-					image.style.display = "none";
+					image.style.display = 'none'
 				}
-			});
+			})
 		}
-	});
-});
+	})
+})
